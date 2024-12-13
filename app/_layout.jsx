2adda7c07a8aa160d5dd6c425253ecb47e12de4c
dudurb17 <eduardo.robettibedin@gmail.com>
2024-12-13 15:42:07 -0300

    created hooks

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,30 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { router, Stack } from "expo-router";
+import { AuthProvider, useAuth } from "../contexts/AuthContext";
+import { supabase } from "../lib/supabase";
 
 export default function _layout() {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  );
+}
+
+function MainLayout() {
+  const { setAuth } = useAuth();
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log("Session user: ", session?.user.id);
+
+      if (session) {
+        setAuth(session?.user);
+        router.replace("/home");
+      } else {
+        setAuth(null);
+        router.replace("/welcome");
+      }
+    });
+  }, []);
   return <Stack screenOptions={{ headerShown: false }} />;
 }
---
+++ contexts/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+
+
+const AuthContext = createContext()
+
+export const AuthProvider =({children})=>{
+  const [user, setUser] = useState();
+
+  const setAuth = authUser =>{
+    setUser(authUser?? 'sem dado');
+  }
+
+  const setUserData = userData =>{
+    setUser({...userData})
+  }
+
+  return (
+    <AuthContext.Provider value={{user, setAuth, setUserData}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth =()=> useContext(AuthContext)
