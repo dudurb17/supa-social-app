d71980205ed9514e773a0f36ff0400e1a920b415
dudurb17 <eduardo.robettibedin@gmail.com>
2024-12-16 16:31:06 -0300

    created screens home. profile, newPost and notifications

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,12 +1,18 @@
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import Button from "../../components/Button";
 import { useAuth } from "../../contexts/AuthContext";
 import { supabase } from "../../lib/supabase";
+import { hp, wp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
+import Icon from "../../assets/icons";
+import { useRouter } from "expo-router";
+import Avatar from "../../components/Avatar";
 
 export default function Home() {
   const { user } = useAuth();
+  const router = useRouter();
 
   console.log("user: ", user);
 
@@ -18,10 +24,84 @@ export default function Home() {
   };
   return (
     <ScreenWrapper>
-      <Text>H</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>LinkUp</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={() => router.push("notifications")}>
+              <Icon
+                name="heart"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("newPost")}>
+              <Icon
+                name="plus"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("profile")}>
+              <Avatar
+                uri={user?.iamge}
+                size={hp(4.3)}
+                rounded={theme.radius.sm}
+                style={{ borderWidth: 2 }}
+              />
+            </Pressable>
+          </View>
+        </View>
+      </View>
       <Button title="logout" onPress={onLogout} />
     </ScreenWrapper>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    marginHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold,
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+    borderWidth: 3,
+  },
+  icons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 18,
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4),
+  },
+  noPosts: {
+    fontSize: hp(2),
+    textAlign: "center",
+    color: theme.colors.text,
+  },
+  pill: {
+    position: "absolute",
+    right: -10,
+    top: -4,
+  },
+});
---
+++ app/(main)/newPost.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function NewPost() {
+  return (
+    <View>
+      <Text>NewPost</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
